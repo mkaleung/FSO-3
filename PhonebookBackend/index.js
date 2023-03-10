f230cdef55ff0acf4874c7edbe78e6eaa3ed7331
jincrypt <leung.mka@gmail.com>
2023-03-10 17:47:02 -0500

    implemented validation on backend (mongoose) and displayed error on frontend

--- PhonebookBackend/index.js
+++ PhonebookBackend/index.js
@@ -15,38 +15,15 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error : error.message })
+    
   }
 }
-//This has to be the last loaded middleware
-app.use(errorHandler)
-
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(person => 
     response.json(person))
-})
+}) 
 
 const generateInfoPage = (persons) => {
   const date = new Date()
@@ -86,31 +63,25 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 })
 
-// const generateId = () => {
-//   const randomId = parseInt(Math.random() * 1000000)
-//   console.log(randomId)
-//   return randomId
-// }
-
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
   const person = {
-    name: body.name,
-    number: body.number,
+    name: name,
+    number: number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (body.content === undefined) {
+  if (body.name === '') {
     return response.status(400).json({ error: 'content missing' })
   }
 
@@ -119,11 +90,15 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   }) 
 
-  person.save().then(savedPerson => {
+  person.save()
+  .then(savedPerson => {
     response.json(savedPerson)
-  })  
+  }) 
+  .catch(error => next(error)) 
 })
 
+//This has to be the last loaded middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
